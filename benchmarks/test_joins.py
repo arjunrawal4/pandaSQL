33a971e1af3ac122cce2243c2b1e63ef1626a032
Arjun Rawal <arjunrawal4@gmail.com>
2020-07-26 19:11:16 -0500

    updating to guess size of joins

--- benchmarks/test_joins.py
+++ benchmarks/test_joins.py
@@ -16,13 +16,34 @@ def make_registrar():
 
 reg = make_registrar()
 
+def merge_size(left_frame, right_frame, group_by):
+    left_groups = left_frame.groupby(group_by).size()
+    right_groups = right_frame.groupby(group_by).size()
+    left_keys = set(left_groups.index)
+    right_keys = set(right_groups.index)
+    intersection = right_keys & left_keys
+    left_diff = left_keys - intersection
+    right_diff = right_keys - intersection
+    
+    # if the joining column contains np.nan values, these get missed by the intersections
+    # but are present in the merge. These need to be added separately.
+    left_nan = len(left_frame.query('{0} != {0}'.format(group_by)))
+    right_nan = len(right_frame.query('{0} != {0}'.format(group_by)))
+    left_nan = 1 if left_nan == 0 and right_nan != 0 else left_nan
+    right_nan = 1 if right_nan == 0 and left_nan != 0 else right_nan
+    
+    sizes = [(left_groups[group_name] * right_groups[group_name]) for group_name in intersection]
+    sizes += [left_groups[group_name] for group_name in left_diff]
+    sizes += [right_groups[group_name] for group_name in right_diff]
+    sizes += [left_nan * right_nan]
+    return sum(sizes)
 
 @reg
 def join(module, df_a, df_b, n=None, **kwargs):
     assert(isinstance(df_a, module.DataFrame))
     assert(isinstance(df_b, module.DataFrame))
 
-    selection = module.merge(df_a, df_b, on=['c1'])
+    selection = module.merge(df_a, df_b, on=['c0'])
 
     if n is not None:
         selection = selection.head(n=n)
@@ -42,13 +63,22 @@ def run_join_benchmark(benchmark, nrows, file_base, runs, percent=100, limit=Non
     start = time.time()
     df_a = pandas.read_csv(file_base + '_a.csv',  nrows=nrows)
     df_b = pandas.read_csv(file_base + '_b.csv',  nrows=nrows)
-
+    
+    # testing string join
+    df_a['c0'] = df_a['c0'].astype(str)
+    df_b['c0'] = df_b['c0'].astype(str)
+    
     df_b = df_b[df_b['c1'] <= 100_000_000 * percent/100.0]
-
+   
     time_taken = time.time() - start
     stats['pandas']['read_time'] = time_taken
     stats['pandas']['run_time'] = []
 
+    start = time.time()
+    print(merge_size(df_a, df_b, 'c0'))
+    time_taken = time.time() - start
+    print(time_taken) 
+    
     for _ in range(0,runs):
         start = time.time()
         res = func(pandas, df_a, df_b, n=limit,)
